75f25e9573076b00eb2d4a0ee46c24c4c7b76306
Ikenna Okpala <ikennaokpala@gmail.com>
2012-07-09 21:08:13 +0100

    Include railtie to rails

--- lib/debot.rb
+++ lib/debot.rb
@@ -1,1 +1,2 @@
 require 'debot/debot_recipes'
+require 'debot/railtie' if defined?(Rails)
--- lib/debot/railtie.rb
+++ lib/debot/railtie.rb
@@ -1,7 +1,9 @@
+require 'debot'
+require 'rails'
 module Debot
   class Railtie < Rails::Railtie
     rake_tasks do
-      load File.expand_path("../tasks/*.rake",__FILE__)
+      load File.expand_path("../tasks/debot.rake",__FILE__)
     end
   end
 end
