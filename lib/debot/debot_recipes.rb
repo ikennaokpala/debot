9af96545d1027199e0b25d8aa7ce74d6d5b78510
Ikenna Okpala <ikennaokpala@gmail.com>
2012-07-11 03:13:49 +0100

    Workaround for Capistrano issue 168 https://github.com/capistrano/capistrano/issues/168#issuecomment-4144687

--- lib/debot/debot_recipes.rb
+++ lib/debot/debot_recipes.rb
@@ -1,4 +1,10 @@
 require 'capistrano'
 require 'capistrano/cli'
 
+Capistrano::Configuration::Namespaces::Namespace.class_eval do
+  def capture(*args)
+    parent.capture *args
+  end
+end #this is a work around for issue 168 https://github.com/capistrano/capistrano/issues/168#issuecomment-4144687
+
 Dir[File.expand_path('../recipes/*.rb', __FILE__)].sort.each { |f| load f }
