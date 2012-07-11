4d362cb619b0e9ab3b39204bdc3262cab67829c9
Ikenna Okpala <ikennaokpala@gmail.com>
2012-07-11 01:56:59 +0100

    refactor utils to fix duplicate bugs

--- lib/debot/helpers/utils.rb
+++ lib/debot/helpers/utils.rb
@@ -1,7 +1,15 @@
+require 'erb'
+
+def template_rake(from, to)
+  File.open(to, 'w') { |f| f.write(ERB.new(erb_path(from)).result(binding)) } 
+end
+
 def template(from, to)
-  require 'erb'
-  erb = File.read(File.expand_path("../../../../generators/templates/#{from}", __FILE__))
-  File.open(to, 'w') { |f| f.write(ERB.new(erb).result(binding)) } 
+  put ERB.new(erb_path(from)).result(binding), to
+end
+
+def erb_path(from)
+  File.read(File.expand_path("../../../../generators/templates/#{from}", __FILE__))
 end
 
 def set_default(name, *args, &block)
@@ -29,41 +37,36 @@ module Setup
     @number_of_releases = ask("How many releases would like to keep?")
     @scm = ask("Your choosen scm (git or svn)?")
     @repository = ask("Enter your repository's url/location:")
-    @app_parent_directory = ask("What is the parent directory for this project? (Given full path details)")
-    @stage_names = []
 
     begin
       puts "Provide details for creating a multi-stage file:"
       puts
       @stage_name = ask("What would you like to call this stage file?")
-      @stage_names << @stage_name
       @domain = ask("What is your #{@stage_name} domain name?")
+      @app_parent_directory = ask("What is the parent directory for #{@stage_name} stage? (Given full path details)")
       @deploy_to = ask("Where on your server would #{@stage_name} be located? (Give file path)")
       @branch = ask("What branch would you like to deploy for this stage?")
-    end while(ask?("Would you like to create a (another) stage file? (yes/no)"))
-
-    deploy_directory = File.join(Rails.root, "config")
-
-    @stage_names.each do |stage_name|
       stages_directory = File.join(Rails.root, "config/deploy")
 
       puts "Creating stage file #{stages_directory}/#{stage_name}.rb"
 
       if File.directory?(stages_directory)
-        template("stages.rb.erb", "#{stages_directory}/#{stage_name}.rb")
+        template_rake("stages.rb.erb", "#{stages_directory}/#{stage_name}.rb")
       else 
         system "mkdir -p #{stages_directory}"
-        template("stages.rb.erb", "#{stages_directory}/#{stage_name}.rb")
+        template_rake("stages.rb.erb", "#{stages_directory}/#{stage_name}.rb")
       end
-    end
+    end while(ask?("Would you like to create a (another) stage file? (yes/no)"))
+
+    deploy_directory = File.join(Rails.root, "config")
 
     puts "Creating your deploy file #{deploy_directory}/deploy.rb"
 
-    template("deploy.rb.erb", "#{deploy_directory}/deploy.rb")
+    template_rake("deploy.rb.erb", "#{deploy_directory}/deploy.rb")
 
   end
 
-private
+  private
   def self.process(question)
     puts question
     STDIN.gets.chomp.downcase #returns user input
