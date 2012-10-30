bd552803b67c5c7c67fc1e5e6a6b2252ec63b276
Ikenna Okpala <ikennaokpala@gmail.com>
2012-10-30 01:22:04 +0000

    setup hstore after database is created

--- lib/debot/recipes/postgresql.rb
+++ lib/debot/recipes/postgresql.rb
@@ -17,7 +17,9 @@ begin
       desc "Create a database for this application."
       task :create_database, roles: :db, only: {primary: true} do
         run %Q{#{sudo} -u postgres psql -c "create user #{postgresql_user} with password '#{postgresql_password}';"}
+        # run %Q{#{sudo} -u postgres psql -c "alter user #{postgresql_user} with superuser;"}
         run %Q{#{sudo} -u postgres psql -c "create database #{postgresql_database} owner #{postgresql_user};"}
+        run %Q{#{sudo} -u postgres psql -c "create extension if not exists hstore;"}
       end
       after "deploy:setup", "postgresql:create_database"
 
