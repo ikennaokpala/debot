6784362d9158454f44bbf1d41fabf6fedaaeeb55
Ikenna Okpala <ikennaokpala@gmail.com>
2012-10-30 07:33:42 +0000

    Add memcached recipe to debot

---
+++ lib/debot/recipes/memcached.rb
@@ -0,0 +1,29 @@
+begin
+  Capistrano::Configuration.instance.load do
+    set_default :memcached_memory_limit, 64
+
+    namespace :memcached do
+      desc "Install Memcached"
+      task :install, roles: :app do
+        run "#{sudo} apt-get install memcached"
+      end
+      after "debot:install", "memcached:install"
+
+      desc "Setup Memcached"
+      task :setup, roles: :app do
+        template "memcached.erb", "/tmp/memcached.conf"
+        run "#{sudo} mv /tmp/memcached.conf /etc/memcached.conf"
+        restart
+      end
+      after "deploy:setup", "memcached:setup"
+
+      %w[start stop restart].each do |command|
+        desc "#{command} Memcached"
+        task command, roles: :app do
+          run "service memcached #{command}"
+        end
+      end
+    end
+  end
+rescue
+end
